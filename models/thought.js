a5b093e3a06a3812451ff1df92fafe861c77364c
Rscime <Rachelscime@gmail.com>
2023-09-05 19:49:11 -0400

    completed thought model

---
+++ models/thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema (
+{
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxkength: 280,
+    },
+    createdAt: {
+        type: Date,
+        //NEED TO FIND A DATE THINGY
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+},
+{
+    toJSON: {
+
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+const Thoughts = model('Thoughts', thoughtSchema);
+module.exports = Thoughts;
--- models/user.js
+++ models/user.js
@@ -43,5 +43,5 @@ userSchema
     return this.friends.length;
   });
 
-const User = model('user', userSchema);
+const User = model('User', userSchema);
 module.exports = User;
